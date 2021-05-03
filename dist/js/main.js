89fb2b8d998dad1c0500c0e90f3d5bca3e12499f
wvwall <waltervelardo@gmail.com>
2021-05-03 17:01:42 +0200

    Select Artist

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,21 +4,21 @@ var app = new Vue({
     el: '#root',
     data: {
       albums : [],
-      search: "",
       albumsGenre : [],
-      select : "All"
+      albumsArtist : [],
+      select : "All",
     },
     methods: {
-        selectGenre: function () {
-          /* scorro albums e pusho generi in array generi */
-            for (let i = 0; i < this.albums.length; i++) {
-              if (!this.albumsGenre.includes(this.albums[i].genre)) {
-                this.albumsGenre.push(this.albums[i].genre)
-              } else {
-                /* console.log("E' gia incluso"); */
+          searchArtist: function () {
+            /* scorro albums e pusho generi in array generi */
+              for (let i = 0; i < this.albums.length; i++) {
+                if (!this.albumsArtist.includes(this.albums[i].author)) {
+                  this.albumsArtist.push(this.albums[i].author)
+                } else {
+                  /* console.log("E' gia incluso"); */
+                }
               }
             }
-          }
     },
     mounted() {
       /* chiamata api con tutti i dati degli album */
